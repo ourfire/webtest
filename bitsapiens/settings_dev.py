dc079bf888735a5faf65c0c852180549e458393b
Alejandro v <daosapiens@alejandrosair2.lan>
2023-03-13 10:18:40 -0600

    settings

---
+++ bitsapiens/settings_dev.py
@@ -0,0 +1,14 @@
+import os
+
+from bitsapiens.settings import BASE_DIR
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        # other options...
+    },
+]
